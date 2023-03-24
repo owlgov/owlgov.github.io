cb7ee238c67d052268b792c3fc7b5d27085e4874
decodexuwu <decodexps4@gmail.com>
2023-03-24 11:26:23 -0400

    Twitch Embed

---
+++ src/js/twitch.js
@@ -0,0 +1,7 @@
+new Twitch.Embed("twitch-embed", {
+    width: 854,
+    height: 480,
+    channel: "firstinspires34",
+    // Only needed if this page is going to be embedded on other websites
+    parent: ["embed.example.com", "othersite.example.com"]
+})
